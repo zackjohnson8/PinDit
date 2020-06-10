437b4b31b3ea3347872097b5bdc7a239be222f9f
zackjohnson8 <zackjohnson8@gmail.com>
2020-06-09 23:21:14 -0600

    Tabs for two different views with map implemented on map screen

--- MapUsage/FirstViewController.swift
+++ MapUsage/FirstViewController.swift
@@ -7,14 +7,82 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class FirstViewController: UIViewController {
-
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    let regionInMeters: Double = 10000
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        checkLocationServices()
     }
-
-
+    
+    func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    func centerViewOnUserLocation() {
+        if let location = locationManager.location?.coordinate {
+            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+            mapView.setRegion(region, animated: true)
+        }
+    }
+    
+    func checkLocationServices() {
+        if CLLocationManager.locationServicesEnabled(){
+            // Setup our location manager
+            setupLocationManager()
+            checkLocationAuthorization()
+        } else {
+            // Show alert letting the user know they have to turn this on.
+            print("Location wasn't given")
+        }
+    }
+    
+    func checkLocationAuthorization() {
+        switch CLLocationManager.authorizationStatus(){
+        case .authorizedWhenInUse:
+            mapView.showsUserLocation = true
+            centerViewOnUserLocation()
+            locationManager.startUpdatingLocation()
+            break
+        case .denied:
+            // Show alert instructing them how to turn on permissions
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+            break
+        case .restricted:
+            // Cannot be put on (show alert to let them know)
+            break
+        case .authorizedAlways:
+            // Probably won't do
+            break
+        @unknown default:
+            fatalError()
+        }
+    }
+    
 }
 
+
+extension FirstViewController: CLLocationManagerDelegate {
+    
+    func locationManger(_ manager: CLLocationManager, didUpdateLocation location: [CLLocation]) {
+        guard let location = location.last else { return }
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion.init(center: center, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    func locationManger(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        checkLocationAuthorization()
+    }
+    
+}
